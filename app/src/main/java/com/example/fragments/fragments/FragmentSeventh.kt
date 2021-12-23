2aa19a4719dc7ca57ab99eb0e60e701fff4c785f
NiniDolidze <84011073+NiniDolidze@users.noreply.github.com>
2021-12-24 00:24:22 +0400

    commit

--- app/src/main/java/com/example/fragments/fragments/FragmentSeventh.kt
+++ app/src/main/java/com/example/fragments/fragments/FragmentSeventh.kt
@@ -1,6 +1,7 @@
 package com.example.fragments.fragments
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,5 +14,20 @@ class FragmentSeventh : Fragment(R.layout.fragment_seventh) {
     private lateinit var editTextNote: EditText
     private lateinit var textView: TextView
 
-
+//    override fun onCreate(view: View, savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        buttonAdd = view.findViewById(R.id.buttonAdd)
+//        editTextNote = view.findViewById(R.id.editTextNote)
+//        textView = view.findViewById(R.id.textView)
+//
+//        buttonAdd.setOnClickListener {
+//
+//            val note = editTextNote.text.toString()
+//            val text = textView.text.toString()
+//            val result = note + "\n" + text
+//            textView.text = result
+//        }
+//
+//    }
 }
